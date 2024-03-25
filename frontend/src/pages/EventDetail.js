384917078ebde47742cd5577e9c42d75a611e4c6
Sathyaboopathi23 <149357957+Sathyaboopathi23@users.noreply.github.com>
2024-03-25 15:56:03 +0530

    Add files via upload

---
+++ frontend/src/pages/EventDetail.js
@@ -0,0 +1,78 @@
+import { Suspense } from "react";
+import {
+  useRouteLoaderData,
+  json,
+  redirect,
+  Await,
+  defer,
+} from "react-router-dom";
+
+import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+
+function EventDetailPage() {
+  const { event, events } = useRouteLoaderData("event-detail");
+
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadEvent) => <EventItem event={loadEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadEvents) => <EventsList events={loadEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
+}
+
+export default EventDetailPage;
+
+async function loadEvent(id) {
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { messge: "Could not fetch details for selected event." },
+      { status: 500 }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.event;
+  }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    return json({ messge: "Could not fetch events." }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ messge: "Could not delete event." }, { status: 500 });
+  }
+  return redirect("/events");
+}
